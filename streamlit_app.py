3585810c0dd5cc00557fe8180c2ad7b12543cb0e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-16 20:36:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+# Set page config
+st.set_page_config(page_title="Streamlit YouTube Channel Dashboard", layout="wide")
 
-st.write('Hello world!')
+df = pd.read_csv("youtube_channel_data.csv")
+
+st.title("🎈 Streamlit YouTube Channel Dashboard")
+
+with st.sidebar:
+    st.header("⚙️ Settings")
+    start_date = st.date_input("Start date", df['DATE_DAY'].min())
+    end_date = st.date_input("End date", df['DATE_DAY'].max())
+
+    time_frame = st.selectbox(
+        "Select time frame",
+        ("Daily", "Cumulative"),
+    )
+
