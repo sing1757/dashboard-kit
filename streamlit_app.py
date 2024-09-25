1e3656775c56be71dab09cb25f333507a27c6686
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-09-24 19:57:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,9 +108,8 @@ def display_metric(col, title, value, df, column, color, time_frame):
 df = load_data()
 
 # Set up input widgets
-sidebar_logo = "images/streamlit-logo-primary-colormark-lighttext.png"
-main_body_logo = "images/streamlit-mark-color.png"
-st.logo(sidebar_logo, icon_image=main_body_logo)
+st.logo(image="images/streamlit-logo-primary-colormark-lighttext.png", 
+        icon_image="images/streamlit-mark-color.png")
 
 with st.sidebar:
     st.title("YouTube Channel Dashboard")
